dfaaf724953d604c0773dbf68ad994a45f1fe92f
Guillaume Fradin <guillaumefradin@gmail.com>
2022-04-26 23:57:04 +0100

    comment out animate function and renderer creation

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ import * as THREE from 'three'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -27,15 +27,15 @@ const loadedData = await loadSceneAsync_scene_01({
 });
 scene.add(loadedData.scene.threejsScene());
 
-function animate() {
-  requestAnimationFrame( animate );
+// function animate() {
+//   requestAnimationFrame( animate );
 
-  loadedData.scene.incrementTime();
+//   loadedData.scene.incrementTime();
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+//   cube.rotation.x += 0.01;
+//   cube.rotation.y += 0.01;
 
-  renderer.render( scene, camera );
-};
+//   renderer.render( scene, camera );
+// };
 
-animate();
+// animate();
