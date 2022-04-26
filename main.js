0a620a944b56d65e68e8435c550db236862327ec
Guillaume Fradin <guillaumefradin@gmail.com>
2022-04-26 23:43:00 +0100

    clean page

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
 
 import * as THREE from 'three'
 
