a41d9f7b42fbc4b296ead8929951b8be5e1d3c98
Guillaume Fradin <guillaumefradin@gmail.com>
2022-10-06 16:44:31 +0100

    upgrade polygonjs to 1.2.31

--- src/polygonjs/scenes/scene_01/autogenerated/loadSceneData.js
+++ src/polygonjs/scenes/scene_01/autogenerated/loadSceneData.js
@@ -1,23 +1,24 @@
-import {SceneDataManifestImporter} from '@polygonjs/polygonjs/dist/src/engine/io/manifest/import/SceneData';
+import { SceneDataManifestImporter } from "@polygonjs/polygonjs/dist/src/engine/io/manifest/import/SceneData";
 const manifest = {
-	properties: '1651013669992',
-	root: '1651013669992',
-	nodes: {
-		geo1: '1651013669992',
-		'geo1/materials1': '1651013669992',
-		perspectiveCamera1: '1651013669992',
-		'perspectiveCamera1/events1': '1651013669992',
-		arrows: '1651013669992',
-		'arrows/actor1': '1651013669992',
-	},
+  properties: "1665070989922",
+  root: "1665070989922",
+  nodes: {
+    geo1: "1665070989922",
+    "geo1/materials1": "1665070989922",
+    perspectiveCamera1: "1651013669992",
+    "perspectiveCamera1/events1": "1665070989922",
+    arrows: "1651013669992",
+    "arrows/actor1": "1665070989922",
+  },
+  shaders: {},
 };
 
 export const loadSceneData_scene_01 = async (options = {}) => {
-	const sceneDataRoot = options.sceneDataRoot || './polygonjs/scenes';
-	return await SceneDataManifestImporter.importSceneData({
-		sceneName: 'scene_01',
-		urlPrefix: sceneDataRoot + '/' + 'scene_01',
-		manifest: manifest,
-		onProgress: options.onProgress,
-	});
+  const sceneDataRoot = options.sceneDataRoot || "./polygonjs/scenes";
+  return await SceneDataManifestImporter.importSceneData({
+    sceneName: "scene_01",
+    urlPrefix: sceneDataRoot + "/scene_01",
+    manifest: manifest,
+    onProgress: options.onProgress,
+  });
 };
